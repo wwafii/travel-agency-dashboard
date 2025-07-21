e8f9f814b7218f9bcd480955312a24c3b3ad98df
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-22 01:36:43 +0700

    feat(trip): add new trip form with country selection, travel preferences, and map visualization

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { cn } from '~/lib/utils';
+import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
 
 interface Props {
   title: string;
   description: string;
+  ctaText?: string;
+  ctaUrl?: string;
 }
 
-const Header = ({ title, description }: Props) => {
+const Header = ({ title, description, ctaText, ctaUrl }: Props) => {
   const location = useLocation();
   return (
     <header className="header">
@@ -14,6 +17,15 @@ const Header = ({ title, description }: Props) => {
         <h1 className={cn('text-dark-100', location.pathname === '/' ? 'text-2xl md:text-4xl font-bold' : 'text-xl md:text-2xl font-semibold')}>{title}</h1>
         <p className={cn('text-gray-100 font-normal', location.pathname === '/' ? 'text-base md:text-lg' : 'text-sm md:text-lg')}>{description}</p>
       </article>
+
+      {ctaText && ctaUrl && (
+        <Link to={ctaUrl} className="btn btn-primary">
+          <ButtonComponent type="button" className="button-class !h-11 !w-full md:w-[240px]">
+            <img src="/assets/icons/plus.svg" alt="plus" className='size-5'/>
+            <span className='p-16-semibold text-white'>{ctaText}</span>
+          </ButtonComponent>
+        </Link>
+      )}
     </header>
   );
 };
