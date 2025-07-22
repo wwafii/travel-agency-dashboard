5f19149c36046c7d44acb0a15013af7db0d36f55
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-22 15:14:29 +0700

    feat(trip): add AI trip generation using Google Gemini

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, layout, route } from '@react-router/dev/routes';
 
 export default [
     route('sign-in', 'routes/auth/sign-in.tsx'),
+    route('api/create-trip', 'routes/api/create-trip.ts'),
     layout('routes/admin/admin-layout.tsx', [
         route('dashboard', 'routes/admin/dashboard.tsx'),
         route('all-users', 'routes/admin/all-users.tsx'),
